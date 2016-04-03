f981bdde2347ebb33feefa8f068e5680c30d884f
alexxxmen <alexxxmen1990@gmail.com>
2016-04-03 23:04:06 +0300

    template notes

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,7 +3,7 @@ from Blog import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^article/(?P<article_id>\d+)/$', views.article_details, name='articledetails'),
+    url(r'^article/(?P<article_id>\d+)/$', views.article_details, name='article_detail'),
     url(r'^category/(?P<category_id>\d+)/$', views.articles_by_cat, name='articles_by_cat'),
 
 ]
