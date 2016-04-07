19b346853804c70e6940e507f061ce149be46c0f
alexxxmen <alexxxmen1990@gmail.com>
2016-04-07 20:58:36 +0300

    Create tag, category link's template article_details

--- Blog/views.py
+++ Blog/views.py
@@ -5,22 +5,45 @@ from Blog.models import Article, Category, Tag
 
 def home(request):
     articles = Article.objects.all()
-    categories = Category.objects.all()
     context = {
         'articles': articles,
-        'categories': categories,
-        'title': 'Start App'
+        'title': 'Start App',
+        'active_menu': 'home'
     }
     return render(request, 'blog/index.html', context)
 
 
 def article_details(request, article_id):
-    pass
+    try:
+        article = Article.objects.get(pk=article_id)
+        context = {
+            'article': article,
+            'title': 'Start App'
+        }
+        return render(request, 'blog/article_deteils.html', context)
+    except Article.DoesNotExist:
+        raise Http404
 
 
 def articles_by_cat(request, category_id):
-    pass
+    try:
+        articles = Article.objects.filter(category=category_id)
+        context = {
+            'articles': articles,
+            'title': 'Start App'
+        }
+        return render(request, 'blog/index.html', context)
+    except Article.DoesNotExist:
+        raise Http404
 
 
 def articles_by_tag(request, tag_id):
-    pass
+    try:
+        articles = Article.objects.filter(tags__id=tag_id)
+        context = {
+            'articles': articles,
+            'title': 'Start App'
+        }
+        return render(request, 'blog/index.html', context)
+    except Article.DoesNotExist:
+        raise Http404
