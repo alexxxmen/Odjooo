d7fad402aaa6d558d8a45097c2bc69051cd4e925
alexxxmen <alexxxmen1990@gmail.com>
2016-04-09 15:29:23 +0300

    Admin fields Add slug

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from Blog.models import Article, Category, Tag
 
 
-admin.site.register(Article)
-admin.site.register(Category)
-admin.site.register(Tag)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'status', 'pub_date')
+    list_display_links = ('title',)
+    search_fields = ['title', 'slug', 'pub_date', 'modified', 'category', 'tags']
+    list_filter = ['pub_date', 'status', 'category']
+    date_hierarchy = 'pub_date'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- Blog/models.py
+++ Blog/models.py
@@ -5,26 +5,34 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name="Тег")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='slug')
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, help_text='Category name')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='slug')
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
 
 class Article(models.Model):
+    ARTICLE_STATUS = (
+        ('D', 'Черновик'),
+        ('P', 'Опубликовано'),
+        ('E', 'Устарел'),
+    )
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     text = models.TextField()
-    slug = models.SlugField(unique=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='slug')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='categories')
     tags = models.ManyToManyField(Tag, related_name='tags', related_query_name='tag')
+    status = models.CharField(max_length=1, choices=ARTICLE_STATUS, default='D')
 
-    def __str__(self):
+    def __unicode__(self):
         return self.title
